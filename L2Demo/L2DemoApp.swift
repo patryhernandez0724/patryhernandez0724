89afdcc42bd4bb1f888bee0cbd7144c3e873f629
Patricia Hernandez <patryhernandez0724@gmail.com>
2024-08-27 14:14:07 -0400

    Initial Commit

---
+++ L2Demo/L2DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L2DemoApp.swift
+//  L2Demo
+//
+//  Created by Patricia on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct L2DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
