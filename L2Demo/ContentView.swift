07ac28f9bd09c5921dfad7a4d6c0dadd3fe6e63d
Patricia Hernandez <patryhernandez0724@gmail.com>
2024-08-27 22:58:09 -0400

    Test App

--- L2Demo/ContentView.swift
+++ L2Demo/ContentView.swift
@@ -9,16 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0){
+                Image("niagarafalls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                HStack(alignment: .center) {
+                    
+                        Text("Niagara Falls")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Spacer()
+                    VStack{
+                        HStack{
+                            
+                            
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        
+                        Text("(Reviews 361)")
+                            .font(.subheadline)
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                }
+                    
+                    Text("Come visit the falls for an experience of a lifetime.")
+                HStack{
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.gray)
+                
+                }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+                .cornerRadius(15)
+                .shadow(radius: 15)
+                
+            .padding()
+            
+                
         }
-        .padding()
+       
+        }
     }
-}
+    
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
